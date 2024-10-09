691f19653e60cb8ebcf6e6652e4b74c1ec0a866c
Muhammad Shahid Sandhu <39005046+ShahidSandhu@users.noreply.github.com>
2024-10-09 17:30:02 +0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const cors = require('cors');
-const bodyParser = require('body-parser'); // Using body-parser directly
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3030;
 
