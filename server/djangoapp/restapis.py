be586765db516a05fdecff8960f36e6d3d566063
Muhammad Shahid Sandhu <39005046+ShahidSandhu@users.noreply.github.com>
2024-10-10 11:24:31 +0500

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -7,18 +7,21 @@ load_dotenv()
 
 # Set up URLs with default values
 backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+sentiment_analyzer_url = os.getenv('sentiment_analyzer_url',
+                                   default="http://localhost:5050/")
 
 
 def get_request(endpoint, **kwargs):
     """Make a GET request to the backend with optional parameters."""
-    params = "&".join([f"{key}={value}" for key, value in kwargs.items()]) if kwargs else ""
+    params = "&".join([f"{key}={value}" for key,
+                       value in kwargs.items()]) if kwargs else ""
     request_url = f"{backend_url}{endpoint}?{params}"
     print(f"GET from {request_url}")
 
     try:
         response = requests.get(request_url)
-        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+        response.raise_for_status()
+        # Raise an exception for 4xx/5xx status codes
         return response.json()
     except requests.RequestException as err:
         print(f"Network exception occurred: {err}")
@@ -30,7 +33,8 @@ def analyze_review_sentiments(text):
     request_url = f"{sentiment_analyzer_url}analyze/{text}"
     try:
         response = requests.get(request_url)
-        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+        response.raise_for_status()
+        # Raise an exception for 4xx/5xx status codes
         return response.json()
     except requests.RequestException as err:
         print(f"Network exception occurred: {err}")
@@ -42,7 +46,8 @@ def post_review(data_dict):
     request_url = f"{backend_url}/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
-        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+        response.raise_for_status()
+        # Raise an exception for 4xx/5xx status codes
         print(response.json())
         return response.json()
     except requests.RequestException as err:
