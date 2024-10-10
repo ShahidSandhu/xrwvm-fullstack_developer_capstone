4e762fce83362724c663e297aa422f348972d2b4
Muhammad Shahid Sandhu <39005046+ShahidSandhu@users.noreply.github.com>
2024-10-10 14:56:34 +0500

    # pylint: disable = line-too-long

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,4 +1,4 @@
-skip errors and warnings (e.g. E4,W) (default:E121,E123,E126,E226,E24,E704,W503)
+# pylint: disable = line-too-long
 """djangoproj URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
