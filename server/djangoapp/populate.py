fb423c6f0d1c8740ffb5b1b52d512032d5260e77
Muhammad Shahid Sandhu <39005046+ShahidSandhu@users.noreply.github.com>
2024-10-10 15:53:37 +0500

    # pylint: disable-all # --ignore=errors      skip errors and warnings (e.g. E4,W) (default:E121,E123,E126,E226,E24,E704,W503)

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,4 +1,5 @@
-# type: ignore[E501,C0301,E121,E123,E126,E226,E24,E704,W503]
+# pylint: disable-all
+# --ignore=errors      skip errors and warnings (e.g. E4,W) (default:E121,E123,E126,E226,E24,E704,W503)
 from .models import CarMake, CarModel
 
 
