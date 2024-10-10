7ffceeb0079dfefb7477854eddfc547685107022
Muhammad Shahid Sandhu <39005046+ShahidSandhu@users.noreply.github.com>
2024-10-10 12:22:09 +0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -1,4 +1,4 @@
-# pylint: skip-file
+# pylint: disable = line-too-long
 """
 Django settings for djangoproj project.
 
@@ -31,14 +31,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://sandhufia-8000.theianext-1-labs-prod-misc-tools-us-east-0.
-        proxy.cognitiveclass.ai'
+    'https://sandhufia-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
     ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://sandhufia-8000.theianext-1-labs-prod-misc-tools-us-east-0.
-        proxy.cognitiveclass.ai',
-    'https://sandhufia-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.
-        proxy.cognitiveclass.ai'
+    'https://sandhufia-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://sandhufia-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
 
 REST_FRAMEWORK = {
