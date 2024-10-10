465bbc29156267ba1f9c9f9e0cfd7935a4a6894e
Muhammad Shahid Sandhu <39005046+ShahidSandhu@users.noreply.github.com>
2024-10-10 11:52:43 +0500

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,4 +1,4 @@
-# pylint: disable-all
+#pylint: disable = line-too-long
 """djangoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -24,18 +24,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html"), name='home'),
-    path('about/', TemplateView.as_view(
-        template_name="About.html"), name='about'),
-    path('contact/', TemplateView.as_view(
-        template_name="Contact.html"), name='contact'),
-    path('login/', TemplateView.as_view(
-        template_name="index.html"), name='login'),
-    path('register/', TemplateView.as_view(
-        template_name="index.html"), name='register'),
-    path('dealers/', TemplateView.as_view(
-        template_name="index.html"), name='dealers'),
-    path('dealer/<int:dealer_id>/', TemplateView.as_view(
-        template_name="index.html"), name='dealer_detail'),
-    path('postreview/<int:dealer_id>/', TemplateView.as_view(
-        template_name="index.html"), name='post_review'),
+    path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
+    path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
+    path('login/', TemplateView.as_view(template_name="index.html"), name='login'),
+    path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
+    path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
+    path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='dealer_detail'),
+    path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='post_review'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
